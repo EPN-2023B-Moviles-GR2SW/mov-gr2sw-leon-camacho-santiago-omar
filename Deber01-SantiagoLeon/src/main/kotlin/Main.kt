9c21e02375d53deb93c2a4adbda23d30ffe494b5
SLeonCamacho <74388091+SLeonCamacho@users.noreply.github.com>
2024-01-17 09:07:41 -0500

    CRUD for Reservas

--- Deber01-SantiagoLeon/src/main/kotlin/Main.kt
+++ Deber01-SantiagoLeon/src/main/kotlin/Main.kt
@@ -1,20 +1,34 @@
-import com.google.gson.GsonBuilder
-import com.google.gson.reflect.TypeToken
 import models.entities.Reserva
-import utils.DateDeserializer
-import utils.DateSerializer
-import java.io.FileReader
-import java.util.*
+import models.dao.ReservaDAO
+import java.text.SimpleDateFormat
 
 fun main(args: Array<String>){
-    val gson = GsonBuilder()
-        .registerTypeAdapter(Date::class.java, DateSerializer())
-        .registerTypeAdapter(Date::class.java, DateDeserializer())
-        .create()
+    val reservaDAO = ReservaDAO()
+    val dateFormat = SimpleDateFormat("yyyy-MM-dd")
 
-    val fileReader = FileReader("src/main/kotlin/SampleData/reservas.json")
-    val typeToken = object : TypeToken<List<Reserva>>() {}.type
-    val reservas: List<Reserva> = gson.fromJson(fileReader, typeToken)
+    // Test getAllReservas
+    println("Testing getAllReservas:")
+    reservaDAO.getAllReservas().forEach { println(it) }
 
-    reservas.forEach { println(it) }
+    // Test saveReserva
+    println("\nTesting saveReserva:")
+    val newReserva = Reserva(5, "Test Client", dateFormat.parse("2024-08-01"), dateFormat.parse("2024-08-10"), 3, true)
+    reservaDAO.saveReserva(newReserva)
+    reservaDAO.getAllReservas().forEach { println(it) }
+
+    // Test updateReserva
+    println("\nTesting updateReserva:")
+    val updatedReserva = Reserva(1, "Updated Client", dateFormat.parse("2024-05-12"), dateFormat.parse("2024-05-20"), 5, true)
+    reservaDAO.updateReserva(1, updatedReserva)
+    reservaDAO.getAllReservas().forEach { println(it) }
+
+    // Test deleteReserva
+    println("\nTesting deleteReserva:")
+    reservaDAO.deleteReserva(2)
+    reservaDAO.getAllReservas().forEach { println(it) }
+
+    // Test findReservaById
+    println("\nTesting findReservaById:")
+    val foundReserva = reservaDAO.findReservaById(3)
+    println(foundReserva ?: "Reserva no encontrada.")
 }
--- Deber01-SantiagoLeon/src/main/kotlin/models/dao/ReservaDAO.kt
+++ Deber01-SantiagoLeon/src/main/kotlin/models/dao/ReservaDAO.kt
@@ -1,27 +1,63 @@
 package models.dao
 
-import com.google.gson.GsonBuilder
 import models.entities.Reserva
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
 import utils.DateDeserializer
 import utils.DateSerializer
-import java.util.*
+import java.io.FileReader
+import java.io.FileWriter
+import java.util.Date
 
 class ReservaDAO {
     private val gson = GsonBuilder()
         .registerTypeAdapter(Date::class.java, DateSerializer())
         .registerTypeAdapter(Date::class.java, DateDeserializer())
+        .setPrettyPrinting()
         .create()
 
+    private val filePath = "src/main/kotlin/SampleData/reservas.json"
+
+    fun getAllReservas(): List<Reserva> {
+        val fileReader = FileReader(filePath)
+        val typeToken = object : TypeToken<List<Reserva>>() {}.type
+        return gson.fromJson(fileReader, typeToken) ?: emptyList()
+    }
+
     fun saveReserva(reserva: Reserva) {
-        val jsonData = gson.toJson(reserva)
-        // Write jsonData to file
+        val reservas = getAllReservas().toMutableList()
+        reservas.add(reserva)
+        val fileWriter = FileWriter(filePath)
+        gson.toJson(reservas, fileWriter)
+        fileWriter.close()
     }
 
-    fun readReserva(): Reserva {
-        // Read JSON data from file
-        val jsonData = "sampledata/reservas.json"
-        return gson.fromJson(jsonData, Reserva::class.java)
+    fun updateReserva(id: Int, updatedReserva: Reserva) {
+        val reservas = getAllReservas().toMutableList()
+        val index = reservas.indexOfFirst { it.id == id }
+
+        if (index != -1) {
+            reservas[index] = updatedReserva
+            val fileWriter = FileWriter(filePath)
+            gson.toJson(reservas, fileWriter)
+            fileWriter.close()
+        }
     }
 
-    // Other CRUD operations
+    fun deleteReserva(id: Int) {
+        val reservas = getAllReservas().toMutableList()
+        val index = reservas.indexOfFirst { it.id == id }
+
+        if (index != -1) {
+            reservas.removeAt(index)
+            val fileWriter = FileWriter(filePath)
+            gson.toJson(reservas, fileWriter)
+            fileWriter.close()
+        }
+    }
+
+    fun findReservaById(id: Int): Reserva? {
+        val reservas = getAllReservas()
+        return reservas.find { it.id == id }
+    }
 }
