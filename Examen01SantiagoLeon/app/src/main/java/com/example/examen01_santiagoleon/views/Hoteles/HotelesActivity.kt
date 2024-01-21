9c7c35ea8249ed7f64f28df117fc214b5d61a8ac
SLeonCamacho <74388091+SLeonCamacho@users.noreply.github.com>
2024-01-21 09:17:36 -0500

    Navigation finished

--- Examen01SantiagoLeon/app/src/main/java/com/example/examen01_santiagoleon/views/Hoteles/HotelesActivity.kt
+++ Examen01SantiagoLeon/app/src/main/java/com/example/examen01_santiagoleon/views/Hoteles/HotelesActivity.kt
@@ -1,9 +1,11 @@
 package com.example.examen01_santiagoleon.views.Hoteles
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.example.examen01_santiagoleon.R
+import com.example.examen01_santiagoleon.views.Reservas.VerTodasReservasActivity
 
 class HotelesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +15,12 @@ class HotelesActivity : AppCompatActivity() {
         val btnRegresarHoteles = findViewById<Button>(R.id.btn_regresar_hoteles_menu)
         btnRegresarHoteles.setOnClickListener {
             finish()
+        }
+
+        val btnVerTodosHoteles = findViewById<Button>(R.id.btn_ver_todos_hoteles)
+        btnVerTodosHoteles.setOnClickListener {
+            val intentVerTodosHoteles = Intent(this, VerTodosHotelesActivity::class.java)
+            startActivity(intentVerTodosHoteles)
         }
     }
 }
--- Examen01SantiagoLeon/app/src/main/java/com/example/examen01_santiagoleon/views/Reservas/ReservasActivity.kt
+++ Examen01SantiagoLeon/app/src/main/java/com/example/examen01_santiagoleon/views/Reservas/ReservasActivity.kt
@@ -1,9 +1,11 @@
 package com.example.examen01_santiagoleon.views.Reservas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.example.examen01_santiagoleon.R
+import com.example.examen01_santiagoleon.views.Hoteles.HotelesActivity
 
 class ReservasActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +15,18 @@ class ReservasActivity : AppCompatActivity() {
         val btnRegresarReservas = findViewById<Button>(R.id.btn_regresar_reservas_menu)
         btnRegresarReservas.setOnClickListener {
             finish()
+        }
+
+        val btnVerTodasReservas = findViewById<Button>(R.id.btn_ver_todas_reservas)
+        btnVerTodasReservas.setOnClickListener {
+            val intentVerTodasReservas = Intent(this, VerTodasReservasActivity::class.java)
+            startActivity(intentVerTodasReservas)
+        }
+
+        val btnReservasPorHotel = findViewById<Button>(R.id.btn_reservas_listar_id_hotel)
+        btnReservasPorHotel.setOnClickListener {
+            val intentReservasPorHotel = Intent(this, ReservasPorHotelActivity::class.java)
+            startActivity(intentReservasPorHotel)
         }
     }
 }
