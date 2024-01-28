b10a943f58dc46665f915c1e65671e1be464cad8
SLeonCamacho <74388091+SLeonCamacho@users.noreply.github.com>
2024-01-28 15:58:43 -0500

    Firebase Authentification

--- 02-Android/app/src/main/java/com/example/b2023gr2sw/MainActivity.kt
+++ 02-Android/app/src/main/java/com/example/b2023gr2sw/MainActivity.kt
@@ -114,6 +114,12 @@ class MainActivity : AppCompatActivity() {
             .setOnClickListener {
                 irActividad(GGoogleMapsActivity::class.java)
             }
+
+        val botonFireBaseUI = findViewById<Button>(R.id.btn_intent_firebase_ui)
+        botonFireBaseUI
+            .setOnClickListener {
+                irActividad(HFirebaseUIAuth::class.java)
+            }
     } // Termina create
 
     fun abrirActividadConParametros(
