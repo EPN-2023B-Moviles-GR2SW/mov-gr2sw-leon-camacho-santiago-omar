9d9712a7e409c9a4db9838bdced2e564f5988a60
SLeonCamacho <74388091+SLeonCamacho@users.noreply.github.com>
2024-02-02 09:34:04 -0500

    Deber02

---
+++ Deber02SantiagoLeon/app/src/main/java/com/example/deber02_santiagoleon/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.deber02_santiagoleon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
