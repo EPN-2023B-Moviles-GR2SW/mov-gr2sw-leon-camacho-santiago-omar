8ab6f24bddf1fcbab945bdc6362d9ee368e5b4d7
SLeonCamacho <74388091+SLeonCamacho@users.noreply.github.com>
2024-03-01 16:10:55 -0500

    Examen02-Created

---
+++ Examen02SantiagoLeon/app/src/main/java/com/example/examen02_santiagoleon/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.examen02_santiagoleon
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.example.examen02_santiagoleon.controllers.MenuActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val buttonEnterMenu = findViewById<Button>(R.id.btn_entrar_menu)
+        buttonEnterMenu.setOnClickListener {
+            val intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
